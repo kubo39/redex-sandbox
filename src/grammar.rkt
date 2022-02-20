ba6793cfcb96e9c77fc86a2c8f1a2a857ee2348c
Hiroki Noda <kubo39@gmail.com>
2022-02-21 00:14:48 +0900

    expression reform

--- src/grammar.rkt
+++ src/grammar.rkt
@@ -10,73 +10,22 @@
   (env ([x type] env)
        ())
 
-  (expression
-   assign-expression)
-
-  (assign-expression
-   (conditional-expression assign-op assign-expression)
-   conditional-expression)
-
-  (conditional-expression
-   oror-expression)
-
-  (oror-expression
-   andand-expression
-   (oror-expression oror-op andand-expression))
-
-  (andand-expression
-   or-expression
-   (andand-expression andand-op or-expression))
-
-  ; currently or-expression is not implemented.
-  (or-expression
-   xor-expression)
-   ;; (| or-expression xor-expression))
-
-  (xor-expression
-   and-expression
-   (xor-expression xor-op and-expression))
-
-  (and-expression
-   cmp-expression
-   (and-expression and-op cmp-expression))
-
-  (cmp-expression
-   equal-expression
-   shift-expression)
-
-  (equal-expression
-   (shift-expression equal-op shift-expression))
-
-  (shift-expression
-   add-expression
-   (shift-expression shift-op add-expression))
-
-  (add-expression
-   mul-expression
-   (add-expression add-op mul-expression))
-
-  (mul-expression
-   unary-expression
-   (mul-expression mul-op unary-expression))
-
-  (unary-expression
-   (unary-op unary-expression)
-   pow-expression)
-
-  (pow-expression
-   postfix-expression
-   (postfix-expression pow-op unary-expression))
-
-  (postfix-expression
-   primary-expression)
-
-  (primary-expression
-   true
-   false
-   integer
-   identifier
-   (expression))
+  (expression ::=
+              true
+              false
+              integer
+              identifier
+              (assign-op expression expression)
+              (oror-op expression expression)
+              (andand-op expression expression)
+              (xor-op expression expression)
+              (and-op expression expression)
+              (equal-op expression expression)
+              (shift-op expression expression)
+              (add-op expression expression)
+              (mul-op expression expression)
+              (unary-op expression expression)
+              (pow-op expression expression))
 
   (assign-op = +=)
   (oror-op ||)
@@ -104,71 +53,63 @@
 ; (a += b)
 (redex-match
  ares
- assign-expression
- (term (a += b))
- )
+ expression
+ (term (+= a b)))
 
 ; (a || b)
 (redex-match
  ares
- oror-expression
- (term (a || b))
- )
+ expression
+ (term (|| a b)))
 
 ; (a && b)
 (redex-match
  ares
- andand-expression
- (term (a && b))
- )
+ expression
+ (term (&& a b)))
 
 ; (a ^ b)
 (redex-match
  ares
- xor-expression
- (term (a ^ b))
- )
+ expression
+ (term (^ a b)))
 
 ; (a & b)
 (redex-match
  ares
- and-expression
- (term (a & b))
- )
+ expression
+ (term (& a b)))
 
 ; (a == b)
 (redex-match
  ares
- equal-expression
- (term (a == b))
- )
+ expression
+ (term (== a b)))
 
 ; (a << b)
 (redex-match
  ares
- shift-expression
- (term (a << b)))
+ expression
+ (term (<< a b)))
 
 ; (a + b)
 (redex-match
  ares
- add-expression
- (term (a + b))
- )
+ expression
+ (term (+ a b)))
 
 ; (a * b)
 (redex-match
  ares
- mul-expression
- (term (a * b))
- )
+ expression
+ (term (* a b)))
 
 ; (a ^^ b)
 (redex-match
  ares
- pow-expression
- (term (a ^^ b))
- )
+ expression
+ (term (^^ a b)))
+
 
 (define-judgment-form ares
   #:mode (types I I O)
@@ -177,15 +118,15 @@
   [(types env expression_1 int)
    (types env expression_2 int)
    ----------------------------
-   (types env (expression_1 add-op expression_2) int)]
+   (types env (add-op expression_1 expression_2) int)]
   [(types env expression_1 int)
    (types env expression_2 int)
    ----------------------------
-   (types env (expression_1 equal-op expression_2) bool)]
+   (types env (equal-op expression_1 expression_2) bool)]
   [(types env expression_1 bool)
    (types env expression_2 bool)
    ----------------------------
-   (types env (expression_1 equal-op expression_2) bool)]
+   (types env (equal-op expression_1 expression_2) bool)]
   [------------------------
    (types env integer int)]
   [---------------------
@@ -196,7 +137,7 @@
 (test-equal
  (judgment-holds
   (types ()
-         (1 + 1)
+         (+ 1 1)
          type)
   type)
  '(int))
@@ -204,7 +145,7 @@
 (test-equal
  (judgment-holds
   (types ()
-         (true + 1)
+         (+ true 1)
          type)
   type)
  '())
@@ -212,7 +153,7 @@
 (test-equal
  (judgment-holds
   (types ()
-         (1 == 1)
+         (== 1 1)
          type)
   type)
  '(bool))
@@ -220,7 +161,7 @@
 (test-equal
  (judgment-holds
   (types ()
-         (true == false)
+         (== true false)
          type)
   type)
  '(bool))
